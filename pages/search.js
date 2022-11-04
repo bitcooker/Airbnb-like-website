9db71f7850ab121c0f7aa561379738bae71c03bf
bitcooker <dreamhighpassionhigh@gmail.com>
2022-11-04 11:21:54 +0200

    add dynamic search result

--- pages/search.js
+++ pages/search.js
@@ -1,16 +1,28 @@
+import { format } from "date-fns";
+import { useRouter } from "next/dist/client/router";
 import React from "react";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 
 function Search() {
+  const router = useRouter();
+
+  const { location, startDate, endDate, noOfGuests } = router.query;
+
+  const formattedStartDate = format(new Date(startDate), "dd MMMM yy");
+  const formattedEndtDate = format(new Date(endDate), "dd MMMM yy");
+  const range = `${formattedStartDate} - ${formattedEndtDate}`;
+
   return (
     <div className="h-screen">
       <Header />
 
       <main className="flex">
-        <section className="flex">
-          <p className="text-xs">300+ Stays for 5 number of guests</p>
-          <h1 className="text-3xl font-semibold mt-2 mb-6">Stays in Mars</h1>
+        <section className="flex-grow pt-14 px-6">
+          <p className="text-xs">300+ Stays - {range} - for {noOfGuests} guests</p>
+          <h1 className="text-3xl font-semibold mt-2 mb-6">
+            Stays in {location}
+          </h1>
 
           <div className="hidden lg:inline-flex mb-5 space-x-3 text-gray-800 whitespace-nowrap">
             <p className="button">Cancellation Flexibility</p>
