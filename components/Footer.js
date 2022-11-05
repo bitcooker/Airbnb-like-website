bb5bf8d8c7b42ddade437e8bc956c3ac67c67402
bitcooker <dreamhighpassionhigh@gmail.com>
2022-11-05 17:44:40 +0200

    update search page

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer() {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100 text-gray-600">
+    <footer className="grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100 text-gray-600">
       <div className="space-y-4 text-xs text-gray-800">
         <h5
           className="
@@ -68,7 +68,7 @@ function Footer() {
         <p>Airbnb Plus</p>
         <p>Airbnb Luxe</p>
       </div>
-    </div>
+    </footer>
   );
 }
 
--- components/InfoCard.js
+++ components/InfoCard.js
@@ -5,7 +5,7 @@ function InfoCard({ img, location, title, description, star, price, total }) {
   return (
     <div className="flex py-7 px-2 border-b cursor-pointer hover:opacity-80 hover:shadow-lg pr-4 transition duration-200 ease-out first:border-t">
       <div className="relative h-24 w-40 md:h-52 md:w-80 flex-shrink-0">
-        <Image src={img} layout="fill" objectFit="cover" />
+        <Image src={img} layout="fill" objectFit="cover" className="rounded-2xl" />
       </div>
 
       <div className="flex flex-col flex-grow pl-5">
